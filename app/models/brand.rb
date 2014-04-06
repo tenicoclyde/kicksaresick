79d452de0188c528500de42eecd2f0fa4e71f845
Nico Te <bluete@Nicos-MacBook-Pro.local>
2014-04-06 17:41:08 -0500

    Added new name for brand and category tables

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,4 @@
 class Brand < ActiveRecord::Base
-  has_many: products
+  has_many  :products
   validates :name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  belongs_to: category
-  belongs_to: brand
+  belongs_to :category
+  belongs_to :brand
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
