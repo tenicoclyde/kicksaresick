6652f232fb282a8f82a5e4e1a2404eb07e84c39e
Nico Te <bluete@Nicos-MacBook-Pro.local>
2014-04-06 17:31:56 -0500

    Added some validations and associations

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,4 @@
 class Brand < ActiveRecord::Base
+  has_many: products
+  validates :name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+  belongs_to: category
+  belongs_to: brand
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+
 end
