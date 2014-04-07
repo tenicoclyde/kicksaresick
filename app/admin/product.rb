fdd30447e00d8ea60611e2d5426eae4b3a5ae7b5
Nico Te <bluete@Nicos-MacBook-Pro.local>
2014-04-06 21:20:42 -0500

    Added brand and category to the active admin

---
+++ app/admin/product.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register Product do
+  index do
+    column :name
+    column :description
+    column :category_id
+    column :brand_id
+    column :quantity
+    column :price do |product|
+        number_to_currency product.price
+    end
+    column :created_at
+    default_actions
+  end
+  
+  permit_params :name, :description, :category_id, :brand_id, :quantity ,:price
+    
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
