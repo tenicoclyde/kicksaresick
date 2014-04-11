f49decae2c4b5d2e094ac93091dbb94987cef40e
Nico Te <bluete@Nicos-MacBook-Pro.local>
2014-04-10 20:32:26 -0500

    installed kaminari for page pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.find(:all, :include => [:category,:brand])
+    @products = Product.find(:all, :include => [:category,:brand]).page(params[:page])
   end
   
   def shoes_category
