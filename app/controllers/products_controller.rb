6ec55b0eb85e2c473538c95cfe65df2e84dd3d28
Nico Te <bluete@Nicos-MacBook-Pro.local>
2014-04-17 13:51:55 -0500

    Ordered by name in ascending for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Kaminari.paginate_array(Product.find(:all, :include => [:category,:brand])).page(params[:page]).per(3)
+    @products = Kaminari.paginate_array(Product.find(:all, :include => [:category,:brand], :order => "name ASC")).page(params[:page]).per(3)
   end
   
   def shoes_category
