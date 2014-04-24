c7dcc0fa2405597f8c7efd32e2483ec7f2b4c93d
Nico Te <bluete@Nicos-MacBook-Pro.local>
2014-04-24 01:39:03 -0500

    Added styles

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
   def index
     @sale_products = Product.where('sale_status_id = 1')
-    @recent_products = Product.order('created_at desc').limit(5).page(params[:page]).per(1)
+    @recent_products = Product.order('created_at desc').limit(5).page(params[:page]).per(3)
   end
 end
