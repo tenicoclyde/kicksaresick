429545f35eddc4f2cc8e3e10659be9180d6aa81b
Nico Te <bluete@Nicos-MacBook-Pro.local>
2014-04-23 22:42:21 -0500

    Modified

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.4'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'sqlite3', group: [:development, :test]
+gem 'pg', group: [:production]
 
 gem 'activeadmin', github: 'gregbell/active_admin'
 
--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,6 +1,22 @@
 ActiveAdmin.register Order do
 
+  index do
+    column :customer_id
+    column :product_id
+    column :shipped_date
+    column :tracking_number
+    column :status
+    column :gst
+    column :grandtotal
+    column :created_at
+    default_actions
+  end
+
+  permit_params :customer_id,  :product_id, :shipped_date, :ship_via, :shipping_company,
+  :tracking_number, :status, :created_at, :updated_at, :gst, :pst, :subtotal, :grandtotal
   
+  
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
